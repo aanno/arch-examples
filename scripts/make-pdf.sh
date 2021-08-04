4827d156ea99f4861818f6563077809937364e30
aanno <aanno@users.noreply.github.com>
2021-08-04 15:46:48 +0200

    not use but require asciidoctor-pdf

--- scripts/make-pdf.sh
+++ scripts/make-pdf.sh
@@ -5,5 +5,5 @@ set -ex
 PLANTUML="$HOME/Downloads/plantuml.1.2021.9.jar"
 
 java -jar "$PLANTUML" -tsvg *.comp.puml
-asciidoctor-pdf -v -b pdf -d article solution.ad
+asciidoctor -r asciidoctor-pdf -v -b pdf -d article solution.ad
 exec evince solution.pdf &
