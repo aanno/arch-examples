da2e9bff291e1e377f118ad807b5436293fa3a9a
aanno <aanno@users.noreply.github.com>
2021-10-13 15:55:15 +0200

    add wr-node.deploy.puml

--- re/scripts/make-pdf.sh
+++ re/scripts/make-pdf.sh
@@ -4,6 +4,6 @@ set -ex
 
 PLANTUML="$HOME/Downloads/plantuml.1.2021.9.jar"
 
-java -jar "$PLANTUML" -tsvg *.comp.puml
+java -jar "$PLANTUML" -tsvg *.comp.puml *.deploy.puml
 # asciidoctor -r asciidoctor-pdf -v -b pdf -d article solution.ad
 # exec evince solution.pdf &
